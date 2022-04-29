d9ca9449df6daff5a9337e70d0a83003bd38631f
Aimar <aimaroribevigo@gmail.com>
2022-04-29 09:32:44 -0500

    tercer commit

--- lab07/src/app.js
+++ lab07/src/app.js
@@ -9,6 +9,15 @@ app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+
 app.use(express.urlencoded({extended: false}));
+
 app.use(require('./routes/index'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next)=>{
+    res.status(404).render('404');
+});
+
+module.exports = app;
