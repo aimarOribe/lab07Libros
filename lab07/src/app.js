217a38b7156bfb75fd3d669023f84fba9aeacb6a
Aimar <aimaroribevigo@gmail.com>
2022-04-29 08:56:28 -0500

    primer commit

---
+++ lab07/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(require('./routes/index'));
+app.use(express.static(path.join(__dirname, 'public')));
